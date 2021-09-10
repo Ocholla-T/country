08263755a87c97cdcd8d66e8e982f35799799444
Ocholla-T <tonnyocholla8@gmail.com>
2021-09-10 20:14:49 +0300

    added module for filter dropdown

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from 'vuex';
 import theme from './modules/theme';
 import search from './modules/search';
+import filter from './modules/filter';
 
 export default createStore({
   modules: {
     theme,
     search,
+    filter,
   },
 });
