cd8a26c96e070d207d2397f61d3e425216c2340c
Ocholla-T <tonnyocholla8@gmail.com>
2021-09-16 10:57:07 +0300

    added actions to filter countries based on if it's all or just from search

--- src/store/modules/filter.js
+++ src/store/modules/filter.js
@@ -1,21 +1,61 @@
+import axios from 'axios';
+
 const state = {
-  isClicked: false,
+  isOpen: false,
+  regions: ['Africa', 'Americas', ' Asia', 'Europe', 'Oceania'],
+  countries: [],
 };
 
 const getters = {
-  isClicked: (state) => state.isClicked,
+  isOpen: (state) => state.isOpen,
+  regions: (state) => state.regions,
 };
 
 const actions = {
-  clicked: ({ state, commit }) => {
-    state.isClicked = !state.isClicked;
+  openDropdown: ({ state, commit }) => {
+    state.isOpen = !state.isOpen;
+    commit('hasBeenOpened', state.isOpen);
+  },
+  commitCountry: ({ commit }, element) => {
+    let country = {
+      name: element.name,
+      population: element.population,
+      region: element.region,
+      capital: element.capital,
+    };
 
-    commit('hasBeenClicked', state.isClicked);
+    commit('setCountries', country);
+    state.countries = [];
+  },
+  filterCountries: async ({ state, dispatch, rootState }, payload) => {
+    let region = payload.target.innerText;
+    let response = await axios.get(`https://restcountries.eu/rest/v2/region/${region}`);
+
+    // filters all countries in the world according to region
+    if (state.countries.length === 0 && rootState.search.country.length === 0) {
+      response.data.forEach((element) => {
+        dispatch('commitCountry', element);
+      });
+    }
+    // filters searched countries according to their region
+    else if (rootState.search.country.length !== 0) {
+      state.countries = [];
+
+      let countriesFilteredByRegion = rootState.search.country.filter(
+        (element) => element.region === `${region}`,
+      );
+
+      // commits the countries to a mutation
+      countriesFilteredByRegion.forEach((element) => {
+        dispatch('commitCountry', element);
+      });
+    }
   },
 };
 
 const mutations = {
-  hasBeenClicked: (state, payload) => (state.isClicked = payload),
+  hasBeenOpened: (state, payload) => (state.isOpen = payload),
+  setCountries: (state, payload) => state.countries.push(payload),
 };
 
 export default { namespaced: true, state, getters, actions, mutations };
